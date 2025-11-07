f79a3a5ccabe2c7a20a4ab707abd1c5146b01e4b
Jenna <jli33@academic.rrc.ca>
2025-11-07 08:21:51 -0600

    Updated page.rb.

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,14 @@
 class Page < ApplicationRecord
+    layout "public"
+
+    validates :title,
+               presence: true,
+               format: { with: /\A[a-zA-Z\s'-]+\z/, message: "can only contain letters, spaces, apostrophes, or hyphens" }
+
+    validates :slug,
+               presence: true,
+               uniqueness: true
+
+    validates :content,
+               presence: true
 end
