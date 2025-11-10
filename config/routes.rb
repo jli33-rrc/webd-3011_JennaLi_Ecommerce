881805f7942e7d347a8fa545ff90671483fb836f
Jenna <jli33@academic.rrc.ca>
2025-11-10 10:43:26 -0600

    Updated routes.rb.

--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resources :categories, only: [:show]
+  resources :products, only: [:show]
 end
