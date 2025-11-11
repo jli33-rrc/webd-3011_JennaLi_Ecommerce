e4520f5d8973fb0283111dc1fead6c88a9636178
Jenna <jli33@academic.rrc.ca>
2025-11-11 13:33:23 -0600

    Updated products_controller.rb.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,5 +4,6 @@ class ProductsController < ApplicationController
   def show
     @category = Category.find_by!("LOWER(REPLACE(category_name, ' ', '-')) = ?", params[:category_name].downcase)
     @product = @category.products.find_by!("LOWER(REPLACE(product_name, ' ', '-')) = ?", params[:product_name].downcase)
+    @featured_products = Product.order("RANDOM()").limit(4)
   end
 end
