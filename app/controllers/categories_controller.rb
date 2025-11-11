f31f80dd435e28e344fa8b39e7464d3eec2ac901
Jenna <jli33@academic.rrc.ca>
2025-11-11 09:28:25 -0600

    Updated categories_controller.rb.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,5 +4,6 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find_by!("LOWER(REPLACE(category_name, ' ', '-')) = ?", params[:category_name].downcase)
     @products = @category.products.includes(:category, image_attachment: :blob)
+    @featured_products = Product.order("RANDOM()").limit(4)
   end
 end
