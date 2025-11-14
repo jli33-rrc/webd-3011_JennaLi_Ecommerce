e82b4e7ca99ea7c82c8c94295dc83df1ddc85483
Jenna <jli33@academic.rrc.ca>
2025-11-14 10:17:43 -0600

    Updated products_controller.rb to add search function.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,23 @@ class ProductsController < ApplicationController
     @product = @category.products.find_by!("LOWER(REPLACE(product_name, ' ', '-')) = ?", params[:product_name].downcase)
     @featured_products = Product.order("RANDOM()").limit(4)
   end
+
+  def search
+    @categories = Category.all
+    @products = Product.all
+
+    if params[:q].present?
+      keyword = "%#{params[:q]}%"
+      @products = @products.where(
+        "product_name LIKE ? OR description LIKE ?",
+        keyword, keyword
+      )
+    end
+
+    if params[:category_id].present? && params[:category_id] != ""
+      @products = @products.where(category_id: params[:category_id])
+    end
+
+    render "pages/home"
+  end
 end
