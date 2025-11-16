1857a9eb363b7e89121054e3fcb7a8c9d9c42af2
Jenna <jli33@academic.rrc.ca>
2025-11-16 09:55:58 -0600

    Updated routes.rb.

--- config/routes.rb
+++ config/routes.rb
@@ -20,10 +20,10 @@ Rails.application.routes.draw do
 
   get "/products/search", to: "products#search", as: :search_products
 
-  get    "/cart",                 to: "cart#show",   as: :cart
-  post   "/cart/add/:id",         to: "cart#add",    as: :add_to_cart
-  patch  "/cart/update/:id",       to: "cart#update", as: :update_cart_item
-  delete "/cart/remove/:id",       to: "cart#remove", as: :remove_cart_item
+  get    "/cart", to: "cart#show",   as: :cart
+  post   "/cart/add/:id", to: "cart#add",    as: :add_to_cart
+  patch  "/cart/update/:id", to: "cart#update", as: :update_cart_item
+  delete "/cart/remove/:id", to: "cart#remove", as: :remove_cart_item
 
   get '/:category_name', to: 'categories#show', as: :category
   get '/:category_name/:product_name', to: 'products#show', as: :product
